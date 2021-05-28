1be3da7d7d5fecfc7310d55156bf91e958305a18
Shannon Hensley <shannonhensley@Shannons-MBP.PK5001Z>
2021-05-28 17:31:53 -0500

    corrected files and added photo

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// identify constants// potion options/ rules 
+
+
+
+
+
+
+
+//variables//
+
+
+
+
+
+
+
+//elements computer will need to reference often// rules 
+
+
+
+
+//add event listeners//  for buttons play and restart  
+
+
+
+//functions// 
+
+
+
+
+// call on  audio for win/loss 
+
+
