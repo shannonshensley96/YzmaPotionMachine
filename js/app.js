c9506e55cd080a4f3920eec981ef30d7d8acc582
Shannon Hensley <shannonhensley@Shannons-MBP.PK5001Z>
2021-06-02 13:36:07 -0500

    Added functions

--- js/app.js
+++ js/app.js
@@ -1,33 +1,86 @@
 // identify constants// potion options/ rules 
-
-
-
-
-
+const potionChoices = {
+    llama: {
+        imgUrl: 'img/llamaPotion.png'
+    },
+    human: {
+        imgUrl: 'img/humanPotion.png'
+    },
+    panther:{
+        imgUrl: 'img/pantherPotion.png'
+    },
+    parrot:{
+        imgUrl: 'img/parrotPotion.png'
+    },
+    whale:{
+        imgUrl: 'img/whalePotion.png'
+    },
+    death:{
+        imgUrl : 'img/deathPotion.png'
+    }
+}
 
 
 //variables//
+let results;
+let winner;
+let lost;
 
 
 
+//elements computer will need to reference often// rules
+
+var sect1 = document.getElementById('sect1');
+
+var sect2 = document.getElementById('sect2');
+
+var sect3 = document.getElementById('sect3');
 
 
-
-
-//elements computer will need to reference often// rules 
-
-
-
-
+results = {
+    sect1: 'llama',
+    sect2: 'llama',
+    sect3: 'llama'
+}
 //add event listeners//  for buttons play and restart  
-document.getElementById("lever") document.addEventListener('click',playGame)
+document.getElementById('Lever').addEventListener('click',playGame);
  
+document.getElementById('Reset').addEventListener('click',resetGame);
 
-//functions// 
+
 
 //create function to randomly choose from the poisons
 
-//
+function getRandomPoison(){
+    choices = ['llama','human','panther','parrot','whale','death'];
+    const randomIndex = Math.floor(Math.random()* 6)
+    return choices[randomIndex]};
+
+  function playGame(){ 
+   results.sect1 = getRandomPoison();
+   results.sect2 = getRandomPoison();
+   results.sect3 = getRandomPoison();
+
+   if (results.sect1 === results.sect2 && results.sect1 === results.sect3){
+       winner = 'Player won!'
+   }else{
+       lost = 'try again'
+   }
+};
+
+
+
+// function playGame(){
+//     console.log('playGame is working')
+
+// };
+
+function resetGame(){
+    console.log('resetGame is working')
+};
+
+
+ 
 
 
 // call on  audio for win/loss 
