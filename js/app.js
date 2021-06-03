18c80c46cbfcc7ee13601d8923fcbdc513907ad5
Shannon Hensley <shannonhensley@Shannons-MBP.PK5001Z>
2021-06-03 11:04:44 -0500

    code from 7/2

--- js/app.js
+++ js/app.js
@@ -3,9 +3,6 @@ const potionChoices = {
     llama: {
         imgUrl: 'img/llamaPotion.png'
     },
-    human: {
-        imgUrl: 'img/humanPotion.png'
-    },
     panther:{
         imgUrl: 'img/pantherPotion.png'
     },
@@ -20,7 +17,7 @@ const potionChoices = {
     }
 }
 
-
+ 
 //variables//
 let results;
 let winner;
@@ -30,11 +27,11 @@ let lost;
 
 //elements computer will need to reference often// rules
 
-var sect1 = document.getElementById('sect1');
+const sect1 = document.getElementById('sect1');
 
-var sect2 = document.getElementById('sect2');
+const sect2 = document.getElementById('sect2');
 
-var sect3 = document.getElementById('sect3');
+const sect3 = document.getElementById('sect3');
 
 
 results = {
@@ -42,6 +39,22 @@ results = {
     sect2: 'llama',
     sect3: 'llama'
 }
+
+resultsImage = {
+    // sect1:{
+    //    imgEl: document.querySelector('results.sect1 img')
+    // },
+    // sect2:{
+    //     resultEl: document.getElementById('#sect2-results'),
+    //     imgEl: document.getElementById('#sect2-results img')
+    // },
+    // sect3:{
+    //     resultEl: document.getElementById('#sect3-results'),
+    //     imgEl:document.getElementById('#sect3-results img')
+    // }
+};
+
+
 //add event listeners//  for buttons play and restart  
 document.getElementById('Lever').addEventListener('click',playGame);
  
@@ -52,39 +65,38 @@ document.getElementById('Reset').addEventListener('click',resetGame);
 //create function to randomly choose from the poisons
 
 function getRandomPoison(){
-    choices = ['llama','human','panther'];
-    //'parrot','whale','death'
-    const randomIndex = Math.floor(Math.random()* 3 )
+    choices = ['llama','panther','parrot','whale','death'];
+    const randomIndex = Math.floor(Math.random()* 5 )
     return choices[randomIndex]};
 
-function playGame(){ 
+function playGame(){
+    console.log("play game is working ")
    results.sect1 = getRandomPoison();
    results.sect2 = getRandomPoison();
    results.sect3 = getRandomPoison();
+   sect1.style.backgroundImage = potionChoices[results.sect1].imgUrl;
+//    let sect2 = potionChoices[results.sect2];
+//    let sect3 = potionChoices[results.sect3];
 
-   if (results.sect1 === results.sect2 && results.sect1 === results.sect3){
+   if (results.sect1 === results.sect1 && results.sect1 === results.sect3){
        winner ='Player won!'
    }else{
-       lost = 'try again'
+       lost ='try again'
    }
-   return winner || lost
+   render()
 };
 
 
-
-// function playGame(){
-//     console.log('playGame is working')
-
-// };
-
-function resetGame(){
+// reset game should set all the values back to blank 
+function resetGame(){ 
     console.log('resetGame is working')
+    results.sect1 = null;
+    results.sect2 = null;
+    results.sect3 = null;
 };
 
-
- 
-
-
-// call on  audio for win/loss 
-
-
+function render(){
+//    var sect1 =  document.getElementById('sect1').innerHTML = potionChoices[results.sect1];
+//    var sect2 =  document.getElementById('sect2')
+//    var sect3 =  document.getElementById('sect3')
+};
